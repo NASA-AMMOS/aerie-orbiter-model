f6e388c9e0cbec1f3ab893044706ecc28b9f4257
Eric Ferguson <eric.w.ferguson@jpl.nasa.gov>
2025-04-30 23:32:10 -0700

    Add constraint directory for storing procedural constraints

--- missionmodel/src/test/java/missionmodel/GeometrySpawnersTest.java
+++ missionmodel/src/test/java/missionmodel/GeometrySpawnersTest.java
@@ -30,51 +30,51 @@ public class GeometrySpawnersTest {
 
   @Test
   void testGeometrySpawners() {
-    final var simulationStartTime = Instant.parse("2033-09-17T00:00:00Z");;
-    final var simulationDuration = Duration.of(24, HOURS);
+    final var simulationStartTime = Instant.parse("2024-01-02T00:00:00Z");;
+    final var simulationDuration = Duration.of(48, HOURS);
 
     // Input configuration
-    // final Integer scid = -660; // VERITAS
+    // final Integer scid = -660; // VERITAShttp://localhost:3000/
     final Configuration geomConfig = Configuration.defaultConfiguration();
 
     // Add Activities to Plan
     final Map<ActivityDirectiveId, ActivityDirective> schedule = new HashMap<>();
 
     // AddSpacecraftEclipses spawner
-    schedule.put(new ActivityDirectiveId(1L), new ActivityDirective(
-      Duration.ZERO,
-      "AddSpacecraftEclipses",
-      Map.of("searchDuration", SerializedValue.of(86400000000L),
-        "observer", SerializedValue.of("-660"),
-        "target", SerializedValue.of("SUN"),
-        "occultingBody", SerializedValue.of("VENUS"),
-        "stepSize", SerializedValue.of(1800000000L),
-        "useDSK", SerializedValue.of(false)),
-      null,
-      true
-    ));
+//    schedule.put(new ActivityDirectiveId(1L), new ActivityDirective(
+//      Duration.ZERO,
+//      "AddSpacecraftEclipses",
+//      Map.of("searchDuration", SerializedValue.of(86400000000L),
+//        "observer", SerializedValue.of("-660"),
+//        "target", SerializedValue.of("SUN"),
+//        "occultingBody", SerializedValue.of("VENUS"),
+//        "stepSize", SerializedValue.of(1800000000L),
+//        "useDSK", SerializedValue.of(false)),
+//      null,
+//      true
+//    ));
 
     // AddOccultations spawner
-    schedule.put(new ActivityDirectiveId(1L), new ActivityDirective(
-      Duration.ZERO,
-      "AddOccultations",
-      Map.of("searchDuration", SerializedValue.of(86400000000L),
-        "observer", SerializedValue.of("DSS-24"),
-        "target", SerializedValue.of("-660"),
-        "occultingBody", SerializedValue.of("VENUS"),
-        "stepSize", SerializedValue.of(1800000000L),
-        "useDSK", SerializedValue.of(false)),
-      null,
-      true
-    ));
+//    schedule.put(new ActivityDirectiveId(1L), new ActivityDirective(
+//      Duration.ZERO,
+//      "AddOccultations",
+//      Map.of("searchDuration", SerializedValue.of(86400000000L),
+//        "observer", SerializedValue.of("DSS-24"),
+//        "target", SerializedValue.of("-660"),
+//        "occultingBody", SerializedValue.of("VENUS"),
+//        "stepSize", SerializedValue.of(1800000000L),
+//        "useDSK", SerializedValue.of(false)),
+//      null,
+//      true
+//    ));
 
     // AddPeriapsis spawner
     schedule.put(new ActivityDirectiveId(1L), new ActivityDirective(
       Duration.ZERO,
       "AddPeriapsis",
       Map.of("searchDuration", SerializedValue.of(86400000000L),
-        "body", SerializedValue.of("-660"),
-        "target", SerializedValue.of("VENUS"),
+        "body", SerializedValue.of("-74"),
+        "target", SerializedValue.of("MARS"),
         "stepSize", SerializedValue.of(1800000000L),
         "maxDistanceFilter", SerializedValue.of(10000.0)),
       null,
